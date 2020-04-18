9ba6fe6b0b43ee702a3b350a505affc78d736500
Leigh Larson <leighlarson4@gmail.com>
2020-04-18 15:41:49 -0600

    Added working win conditions functions to check win/draw for columns, rows, diags, and to add win to player array

--- src/game.js
+++ src/game.js
@@ -13,105 +13,107 @@ class Game {
   gameplayProgression(e) {
     this.playerTurn(e);
     e.target.disabled = true;
-    this.checkRows();
+    this.checkWinConditions();
   }
 
   playerTurn(e) {
     var player = this.turn ? this.player1 : this.player2;
     this.board[e.target.dataset.i] = player.id;
     e.target.innerText = player.token;
-    player = this.turn ? this.player2 : this.player1;
-    gameboardHeader.innerText = `It's ${player.token}'s turn!`;
     this.turn = !this.turn;
+    this.updateGameboardHeader();
   }
+
+  checkWin(groupOf3) {
+    var set = new Set(groupOf3);
+    if (!(set.has(0)) && set.size === 1) {
+      this.updateWinner(Array.from(set)[0]);
+    } else {
+      this.checkDraw();
+    }
+  }
+
+  updateWinner(winner) {
+    var winningPlayer = this[`player${winner}`];
+    winningPlayer.wins.push(this);
+    this.updateGameboardHeader("win", winningPlayer);
+    this.updatePlayerSidebar(winningPlayer);
+    setTimeout(this.resetBoard, 500);
+    setTimeout(this.updateGameboardHeader, 1000);
+    return winningPlayer;
+  }
+
+  checkDraw() {
+    if (!this.board.includes(0)) {
+      this.updateGameboardHeader("draw");
+      setTimeout(this.resetBoard, 1000);
+    }
+  }
+
+  updateGameboardHeader(condition, winningPlayer) {
+    if (condition === "win") {
+      gameboardHeader.innerText = `${winningPlayer.token} wins!`;
+    } else if (condition === "draw") {
+      gameboardHeader.innerText = "It's a draw!";
+    } else {
+      gameboardHeader.innerText = `It's ${this.turn ? "X" : "O"}'s turn!`;
+    }
+  }
+
+  updatePlayerSidebar(winningPlayer) {
+    if (winningPlayer.id === 1) {
+      p1WinCounter.innerText = `${winningPlayer.wins.length} wins`;
+    } else {
+      p2WinCounter.innerText = `${winningPlayer.wins.length} wins`;
+    }
+  }
+
+  // createMiniBoard(miniboard) {
+  //   for (var i = 0; i < winningPlayer.wins; i++) {
+  //     p1WinDisplay.insertAdjacentHTML("afterbegin", miniboard);
+  //   }
+  //   for (var i = 0; i < winningPlayer.wins; i++) {
+  //     p2WinDisplay.insertAdjacentHTML("afterbegin", miniboard);
+  //   }
+  // }
 
   checkRows() {
     var rows = [];
     for (var i = 0; i < this.board.length; i += 3) {
       var slicedRow = this.board.slice(i, i + 3);
-      this.checkSet(slicedRow);
-    }
-  }
-
-  checkSet(groupOf3) {
-    var set = new Set(groupOf3);
-    if (!(set.has(0)) && set.size === 1) {
-      var winner = Array.from(set)[0];
-      var winningPlayer = this[`player${winner}`];
-      winningPlayer.wins.push(this);
-      gameboardHeader.innerText = `${winningPlayer.token} wins!`;
-      if (winner === 1) {
-        p1WinCounter.innerText = `${winningPlayer.wins.length} wins`;
-      } else {
-        p2WinCounter.innerText = `${winningPlayer.wins.length} wins`;
-      }
+      this.checkWin(slicedRow);
     }
   }
 
   checkColumns() {
-    var columns = [];
-    for (var i = 0; i < this.board.length; i+= 4) {
-      var slicedColumn = this.board.slice(i, i + 4);
-      this.checkSet(slicedColumn);
+    var firstColumn = [this.board[0], this.board[3], this.board[6]];
+    var secondColumn = [this.board[1], this.board[4], this.board[7]];
+    var thirdColumn = [this.board[2], this.board[5], this.board[8]];
+    var columns = [firstColumn, secondColumn, thirdColumn];
+    for (var i = 0; i < columns.length; i++) {
+      this.checkWin(columns[i]);
     }
   }
 
+  checkDiagonals() {
+    var forwardDiagonal = [this.board[0], this.board[4], this.board[8]];
+    var backwardDiagonal = [this.board[2], this.board[4], this.board[6]];
+    this.checkWin(forwardDiagonal);
+    this.checkWin(backwardDiagonal);
+  }
+
+  checkWinConditions() {
+    this.checkRows();
+    this.checkColumns();
+    this.checkDiagonals();
+  }
+
+  resetBoard() {
+    this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (var i = 0; i < gameboard.children.length; i++) {
+      gameboard.children[i].innerText = "";
+      gameboard.children[i].disabled = false;
+    }
+  }
 
 }
-
-
-
-
-
-// resetBoard();
-  // A way to detect when a game is a draw (no one has won)
-  // checkDraw() {
-  //   // if no rows / columns / diagonals are 3 in a row,
-  //   gameboardHeader.innerText = "It's a draw!";
-  //   resetBoard();
-  // }
-
-  // A way to save a winning Game’s board data to the correct player’s wins array
-  // saveGameToPlayerWinCount() {
-  //   if (p1 wins) {
-  //     // add mini game to p1's wins array;
-  //   } else if (p2 wins) {
-  //     // add mini game to p2's wins array;
-  //   }
-  //   updatePlayerSidebar();
-  //   resetBoard();
-  // }
-
-  // updatePlayerSidebar() {
-  //   if (p1 wins) {
-  //     p1WinCounter.innerText = "${player1.wins.length} wins";
-  //     // add mini game to p2's wins display:
-  //     // p1WinDisplay.insertAdjacentHTML("afterbegin", gameTemplate);
-  //
-  //   } else if (p2 wins) {
-  //     p2WinCounter.innerText = "${player2.wins.length} wins";
-  //     // add mini game to p2's wins display:
-  //     // p2WinDisplay.insertAdjacentHTML("afterbegin", gameTemplate);
-  //   }
-  // }
-
-  // A way to reset the Game’s board to begin a new game
-  // resetBoard() {
-  //   topLeftButton1.value = "";
-  //   topMidButton2 = "";
-  //   topRightButton3.value = "";
-  //   midLeftButton4.value = "";
-  //   midMidButton5.value = "";
-  //   midRightButton6.value = "";
-  //   botLeftButton7.value = "";
-  //   botMidButton8.value = "";
-  //   botRightButton9.value = "";
-  //   topLeftButton1.disabled = false;
-  //   topMidButton2.disabled = false;
-  //   topRightButton3.disabled = false;
-  //   midLeftButton4.disabled = false;
-  //   midMidButton5.disabled = false;
-  //   midRightButton6.disabled = false;
-  //   botLeftButton7.disabled = false;
-  //   botMidButton8.disabled = false;
-  //   botRightButton9.disabled = false;
